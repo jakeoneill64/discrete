4b7499456ffb5b01452a003b7658e909dbe38f4f
Jake O'Neill <joneill@hyperglance.com>
2022-08-04 01:27:54 +0100

    deployment script working. add libs to build

--- modules/deploy-modules.sh
+++ modules/deploy-modules.sh
@@ -2,19 +2,30 @@
 
 sed=sed
 
-if [[ $(uname) == "Darwin" ]]; then
-   sed=gsed #let not bother with bsd sed
-fi
+git submodule foreach git reset --hard
 
+if [[ $(uname) == "Darwin" ]]; then
+   sed=gsed #lets not bother with bsd sed
+fi
 
 for d in *
 do
+  [[ -d "${d}" ]] || continue
+  cd $d
+  git clean -fd
+  cd ..
+done
+
+$sed -i"" "81,82d" jsoncpp/CMakeLists.txt #remove the lines which prevent us from building in place.
+
+for d in *
+do
+  [[ -d "${d}" ]] || continue
   cd $d
   cmake .
   make
+  cd ..
 done
-
-cd ../
 
 rm -fr ../lib/* ../include/*
 
@@ -27,7 +38,7 @@ mv glad/libglad.a ../lib
 mv glad/include/glad/glad.h ../include/glad/
 
 mv glfw/src/libglfw3.a ../lib
-mv glfw/include/glfw3.h ../include/glfw
+mv glfw/include/GLFW/glfw3.h ../include/glfw
 
 mv glm/glm/libglm_static.a ../lib
 mv glm/glm/*.hpp ../include/gml
