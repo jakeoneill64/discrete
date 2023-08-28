4142118f341a8805678396cb4bfce8b148f01a34
Jake O'Neill <joneill@hyperglance.com>
2023-08-28 23:36:01 +0100

    add conversion operators for all arithmetic types. Move typename declarations from parameter list to Template parameter list for consistency and readability.

--- src/main/util/fixed_point.h
+++ src/main/util/fixed_point.h
@@ -6,12 +6,22 @@
 #define DISCRETE_FIXED_POINT_H
 
 #include <cinttypes>
+#include <cfloat>
 #include <type_traits>
 
-//TODO verify these template args
-//TODO provide specialisations
 //TODO how about fixed_point with different sizes?
-template <std::size_t size, u_int64_t scaling_factor_inverse>
+template <std::size_t number>
+struct is_base_2 : std::integral_constant<
+        bool,
+        true
+        > {};
+
+// implementation details: https://en.wikipedia.org/wiki/Fixed-point_arithmetic
+template <
+        std::size_t size,
+        std::size_t scaling_factor_inverse,
+        bool is_signed = false,
+        typename = typename std::enable_if_t< is_base_2<scaling_factor_inverse>::value> >
 class fixed{
 
 public:
@@ -19,20 +29,84 @@ public:
     using UInt = typename std::conditional<(size == 8), std::uint8_t,
             typename std::conditional<(size == 16), std::uint16_t,
                     typename std::conditional<(size == 32), std::uint32_t,
-                            typename std::conditional<(size == 64), std::uint64_t, //TODO if the last case is not true, we need to create a malformed type
+                            typename std::conditional<(size == 64), std::uint64_t,
                                     void>::type>::type>::type>::type;
 
-    template <typename T>
-    // NOLINTNEXTLINE
-    fixed(const T& number, typename std::enable_if_t<std::is_floating_point_v<T>, int > = 0){
+    // NOLINTBEGIN ======> clang-tidy complains about implicit conversions, which I want to enable.
+
+    template <
+            typename UnsignedFloatingPoint,
+            typename std::enable_if_t<
+                            std::is_floating_point_v<UnsignedFloatingPoint> &&
+                                std::is_unsigned_v<UnsignedFloatingPoint>,
+                            int > = 0>
+    fixed(const UnsignedFloatingPoint& number){
         data = number * scaling_factor_inverse;
     }
 
-    template <typename T>
-    // NOLINTNEXTLINE
-    fixed(const T& number, typename std::enable_if_t<std::is_integral_v<T>, int> = 0){
+    template <
+            typename SignedFloatingPoint,
+            typename std::enable_if_t<
+                        std::is_floating_point_v<SignedFloatingPoint> &&
+                            std::is_signed_v<SignedFloatingPoint>,
+                        int> = 0>
+    fixed(const SignedFloatingPoint& number){
         data = number << logBase2(scaling_factor_inverse);
     }
+
+    template <
+            typename UnsignedInteger,
+            typename std::enable_if_t<
+                        std::is_integral_v<UnsignedInteger> &&
+                                std::is_unsigned_v<UnsignedInteger>,
+                        int> = 0>
+    fixed(const UnsignedInteger&){
+    }
+
+    template <
+            typename SignedInteger,
+            typename std::enable_if_t<
+                        std::is_integral_v<SignedInteger> &&
+                                std::is_signed_v<SignedInteger>,
+                                int> = 0>
+    fixed(const SignedInteger&){
+    }
+
+    template<
+            typename UnsignedInteger,
+            std::enable_if_t
+                    <std::is_integral_v<UnsignedInteger> && !std::is_signed_v<UnsignedInteger>>* = nullptr>
+    operator UnsignedInteger(){
+
+    }
+
+    template<
+            typename SignedInteger,
+            std::enable_if_t
+                    <std::is_integral_v<SignedInteger> && std::is_signed_v<SignedInteger>>* = nullptr>
+    operator SignedInteger(){
+
+    }
+
+    template<
+            typename UnsignedFloatingPoint,
+            std::enable_if_t
+                    <std::is_floating_point_v<UnsignedFloatingPoint> && !std::is_signed_v<UnsignedFloatingPoint>>* = nullptr>
+    operator UnsignedFloatingPoint(){
+
+    }
+
+    template<
+            typename SignedFloatingPoint,
+            std::enable_if_t
+                    <std::is_floating_point_v<SignedFloatingPoint> && std::is_signed_v<SignedFloatingPoint>>* = nullptr>
+    operator SignedFloatingPoint(){
+
+    }
+
+    // NOLINTEND
+
+
 
 //    template <typename Number,typename = std::enable_if_t<std::is_arithmetic_v<Number> > >
 //    fixed<size, scaling_factor_inverse> operator+(Number operand){
